4fbd92e363aff5c5e5df1051d6f03e25682e899b
ayushverma7011 <ayushverma7011@gmail.com>
2026-01-31 12:39:49 +0530

    Updated Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux'; // ✅ Step 1: Import useSelector
+import { useSelector } from 'react-redux'; 
 import logo from "../assets/images/ShoppyGlobe_logo.png";
 import { IoSearchSharp } from "react-icons/io5"; 
 import { BiSolidOffer, BiHelpCircle, BiUser, BiCartAlt } from "react-icons/bi";
 
 function Header() {
-    // ✅ Step 2: Access the cart items from Redux state
     const cartItems = useSelector((state) => state.cart.items);
 
     return (
         <div>
             <nav className='dark:bg-gray-900 text-white flex justify-between px-10 h-20 items-center'>
-                <Link to="/"> {/* ✅ Wrap logo in Link to return home */}
+                <Link to="/"> 
                     <img 
                         className='h-16 w-auto object-contain mix-blend-screen scale-125' 
                         src={logo} 
@@ -22,7 +21,6 @@ function Header() {
                 <ul className='flex'>
                     <div className='flex m-4 items-center'>
                         <span className="mt-1"><IoSearchSharp /></span>
-                        {/* ✅ Link must have a 'to' attribute */}
                         <Link to="/search"><li className='px-2'>SEARCH</li></Link>
                     </div>
 
@@ -32,17 +30,17 @@ function Header() {
                     </div>
 
                     <div className='flex m-4 items-center'>
-                        <span className="mt-1"><BiHelpCircle /></span> {/* ✅ Fixed Icon */}
+                        <span className="mt-1"><BiHelpCircle /></span> 
                         <Link to='/help'><li className='px-2'>HELP</li></Link>
                     </div>
 
                     <div className='flex m-4 items-center'>
-                        <span className="mt-1"><BiUser /></span> {/* ✅ Fixed Icon */}
+                        <span className="mt-1"><BiUser /></span> 
                         <Link to='/signin'><li className='px-2'>SIGN IN</li></Link>
                     </div>
 
                     <div className='flex m-4 items-center'>
-                        <span className="mt-1 text-xl text-yellow-400"><BiCartAlt /></span> {/* ✅ Fixed Icon */}
+                        <span className="mt-1 text-xl text-yellow-400"><BiCartAlt /></span> 
                         <Link to='/cart'>
                             <li className='px-2 font-bold'>
                                 CART ({cartItems.length}) 
